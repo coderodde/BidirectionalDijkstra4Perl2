76cd28405dbe412c983b473391aea592249685f0
Rodion Efremov <coderodd3@yandex.com>
2022-03-05 15:46:32 +0200

    distance_map seems complete.

--- algorithm.c
+++ algorithm.c
@@ -7,6 +7,17 @@
 #include <float.h>
 #include <stdlib.h>
 
+#define CLEANUP                        \
+fibonacci_heap_free(open_forward);     \
+fibonacci_heap_free(open_backward);    \
+unordered_set_free(closed_forward);    \
+unordered_set_free(closed_backward);   \
+unordered_map_free(distance_forward);  \
+unordered_map_free(distance_backward); \
+unordered_map_free(parent_forward);    \
+unordered_map_free(parent_backward);   \
+
+
 static const size_t INITIAL_MAP_CAPACITY = 1024;
 static const float LOAD_FACTOR = 1.3f;
 
@@ -25,12 +36,9 @@ static int equals_function(void* node_1_id, void* node_2_id) {
     return node_1_id == node_2_id;
 }
 
-static int priority_compare_function(void* a, void* b) {
-    double* p_a = (double*) a;
-    double* p_b = (double*) b;
-
-    double num_a = *p_a;
-    double num_b = *p_b;
+static int priority_compare_function(void* p_a, void* p_b) {
+    double num_a = *(double*) p_a;
+    double num_b = *(double*)(p_b);
 
     if (num_a < num_b) {
         return -1;
@@ -41,10 +49,35 @@ static int priority_compare_function(void* a, void* b) {
     }
 }
 
+static list* traceback_path(size_t* p_touch_vertex,
+                            unordered_map* parent_forward,
+                            unordered_map* parent_backward) {
+
+    list* path = list_alloc(100);
+    size_t* p_current_vertex = p_touch_vertex;
+
+    while (p_current_vertex) {
+        list_push_front(path, p_current_vertex);
+        p_current_vertex = unordered_map_get(parent_forward, 
+                                             p_current_vertex);
+    }
+
+    p_current_vertex = unordered_map_get(parent_backward, p_touch_vertex);
+
+    while (p_current_vertex) {
+        list_push_back(path, p_current_vertex);
+        p_current_vertex = unordered_map_get(parent_backward,   
+                                             p_current_vertex);
+    }
+
+    return path;
+}
+
 list* find_shortest_path(Graph* p_graph,
                          size_t source_vertex_id,
                          size_t target_vertex_id,
                          int* p_return_status) {
+
     fibonacci_heap* open_forward;
     fibonacci_heap* open_backward;
 
@@ -58,6 +91,7 @@ list* find_shortest_path(Graph* p_graph,
     unordered_map* parent_backward;
 
     double best_path_length = DBL_MAX;
+    double temporary_path_length;
     size_t* p_touch_node_id = NULL;
     int return_status;
 
@@ -200,15 +234,69 @@ list* find_shortest_path(Graph* p_graph,
         return NULL;
     }
 
+    /* Initialize the state: */
+    if (!fibonacci_heap_add(open_forward, source_vertex_id, (void*) 0)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+    
+    if (!fibonacci_heap_add(open_backward, target_vertex_id, (void*) 0)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+
+    if (!unordered_map_put(distance_forward, source_vertex_id, (void*)0)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+
+    if (!unordered_map_put(distance_backward, target_vertex_id, (void*)0)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+
+    if (!unordered_map_put(parent_forward, source_vertex_id, NULL)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+
+    if (!unordered_map_put(parent_backward, target_vertex_id, NULL)) {
+        CLEANUP;
+        *p_return_status = RETURN_STATUS_NO_MEMORY;
+        return NULL;
+    }
+
+    while (fibonacci_heap_size(open_forward) > 0 &&
+           fibonacci_heap_size(open_backward) > 0) {
+        if (p_touch_node_id) {
+         
+            temporary_path_length = 
+                (double) 
+                unordered_map_get(
+                    distance_forward,
+                    (size_t) fibonacci_heap_min(open_forward))
+                +
+                (double) 
+                unordered_map_get(
+                    distance_backward,
+                    (size_t) fibonacci_heap_min(open_backward));
+
+            if (temporary_path_length > best_path_length) {
+                return traceback_path(p_touch_node_id,
+                                      parent_forward,
+                                      parent_backward);
+            }
+        }
+
+        fibonacci_heap_extract_min(open_forward);
+    }
 
     /* Clean the state: */
-    fibonacci_heap_free(open_forward);
-    fibonacci_heap_free(open_backward);
-    unordered_set_free(closed_forward);
-    unordered_set_free(closed_backward);
-    unordered_map_free(distance_forward);
-    unordered_map_free(distance_backward);
-    unordered_map_free(parent_forward);
-    unordered_map_free(parent_backward);
+    CLEANUP;
     return NULL;
 }
---
+++ distance_map.h
@@ -0,0 +1,51 @@
+#ifndef DOUBLE_UNORDERED_MAP_H
+#define	DOUBLE_UNORDERED_MAP_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    typedef struct distance_map distance_map;
+    typedef struct distnace_map_iterator distance_map_iterator;
+
+    /***************************************************************************
+    * Allocates a new, empty map with given hash function and given equality   *
+    * testing function.                                                        *
+    ***************************************************************************/
+    distance_map* distance_map_alloc
+       (size_t   initial_capacity,
+        float    load_factor,
+        size_t (*hash_function)(void*),
+        bool   (*equals_function)(void*, void*));
+
+    /***************************************************************************
+    * If p_map does not contain the key p_key, inserts it in the map,          *
+    * associates value with it and return NULL. Otherwise updates the value    *
+    * and returns the old value.                                               *
+    ***************************************************************************/
+    bool distance_map_put(distance_map* map, void* key, double value);
+
+    /***************************************************************************
+    * Returns a positive value if p_key is mapped to some value in this map.   *
+    ***************************************************************************/
+    bool distance_map_contains_key(distance_map* map, void* key);
+
+    /***************************************************************************
+    * Returns the value associated with the p_key, or NULL if p_key is not     *
+    * mapped in the map.                                                       *
+    ***************************************************************************/
+    double distance_map_get(distance_map* map, void* key);
+
+    /*************************************************************************
+    Clears entirely the distance map.                    
+    *************************************************************************/
+    void unordered_map_free(distance_map* map
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* UNORDERED_MAP_H */
